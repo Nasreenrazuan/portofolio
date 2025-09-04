14844948925e3754fc48298fbfc5df87ea360204
Nasreen Razuan <mohamadnasreenrazuan@gmail.com>
2025-09-04 15:13:04 +0800

    test13

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/portofolio/",  // 👈 must match your repo name
+  base: "/portofolio/", // ✅ must have trailing slash
   plugins: [react(), tailwindcss()],
 })
